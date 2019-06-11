25f8a72cea2c42878a545538f2b19c0a6459a33f
Angel Banegas <mcinegas@gmail.com>
2019-06-11 15:51:24 +0200

    Day2 arrow function angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {
   Component
 } from '@angular/core';
+import { filterQueryId } from '@angular/core/src/view/util';
 
 @Component({
   selector: 'app-root',
@@ -14,21 +15,37 @@ export class AppComponent {
       'id': 1,
       'name': 'Angel',
       'surname': 'Banegas',
-      'edad': 23
+      'edad': 23,
+      'price': 14.99,
+      'stock': 4
     },
     {
       'type': 'Alumno',
       'id': 2,
       'name': 'Joselito',
       'surname': 'Gonzalez',
-      'edad': 25
+      'edad': 25,
+      'price': 5,
+      'stock': 8
     },
     {
       'type': 'Alumno',
       'id': 3,
       'name': 'Pacolo',
       'surname': 'Pataton',
-      'edad': 0
+      'edad': 0,
+      'price': 3.54,
+      'stock': 10
     }
   ];
+  
+totalItem(){
+  
+  let result2 = this.myItem.reduce( (a , b) =>  a + b.stock, 0 );
+  return result2;
+  
 }
+
+
+}
+ 
