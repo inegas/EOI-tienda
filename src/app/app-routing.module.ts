52678737c08368abe79d4257294ee1294f1a6ec3
Angel Banegas <mcinegas@gmail.com>
2019-06-18 16:31:33 +0200

    btnX y btnEdit modificados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { NotItemComponent } from './components/not-item/not-item.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch:'full'},
   {path: 'home', component: ItemListComponent},
+  {path: 'item/:id', component: EditItemComponent},
   {path: '**', component: NotItemComponent}
 ];
 
