68ae246bdcc1c7f546554b315414e09b4d64d363
Angel Banegas <mcinegas@gmail.com>
2019-06-20 18:09:46 +0200

    Formulario okei, pero no valida

--- src/app/components/form-cart/form-cart.component.ts
+++ src/app/components/form-cart/form-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-form-cart',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormCartComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup;
+  
+  
+  constructor(private fb:FormBuilder) { 
+    this.form = this.fb.group({
+      "firstname": ["", Validators.required],
+      "email": ["", Validators.required],
+      "adress": ["", Validators.required],
+      "city": ["", Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
+
+  
+
 
 }
